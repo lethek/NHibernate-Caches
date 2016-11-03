91abf498edc638a39a37806976b00d9250c17e65
Michael Monsour <michael@lethek.net>
2016-11-03 10:47:21 +1000

    Added and updated the RtMemoryCache projects which existed but were missing from the solution

--- nugetdeploy.bat
+++ nugetdeploy.bat
@@ -1,7 +1,9 @@
 @echo off
 
 nuget pack SysCache\NHibernate.Caches.SysCache\NHibernate.Caches.SysCache.csproj -Build
-nuget push -source https://www.nuget.org/api/v2/package NHibernateX.Caches.SysCache.2.0.0-Alpha1.nupkg %1
-
 nuget pack SysCache2\NHibernate.Caches.SysCache2\NHibernate.Caches.SysCache2.csproj -Build
+nuget pack RtMemoryCache\NHibernate.Caches.RtMemoryCache.csproj -Build
+
+nuget push -source https://www.nuget.org/api/v2/package NHibernateX.Caches.SysCache.2.0.0-Alpha1.nupkg %1
 nuget push -source https://www.nuget.org/api/v2/package NHibernateX.Caches.SysCache2.2.0.0-Alpha1.nupkg %1
+nuget push -source https://www.nuget.org/api/v2/package NHibernateX.Caches.RtMemoryCache.2.0.0-Alpha1.nupkg %1
